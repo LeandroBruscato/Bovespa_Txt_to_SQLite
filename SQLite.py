0af29e31584c0b2fc25889f91582f6a92f9e9c9a
Leandro Bruscato <leobrus@gmail.com>
2019-05-29 21:22:40 +0100

    Create a class SQLite

--- SQLite.py
+++ SQLite.py
@@ -1,30 +1,116 @@
-import sqlite3
+from SQLite import *
 
-CONST_NAME_DATABASE = 'ActionsInfo.db'
-conn=None
-cur=None
+def CreateTables():
+	sql = 'create table if not exists Stock (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, SHORT_NAME TEXT, CODE_OF_TRADING TEXT)'
+	sQLite.ExecuteNonQuery(sql)
+	sql = 'create table if not exists DailyStockInfo (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,IDStock INTEGER NOT NULL,Day TEXT NOT NULL,OpenPrice INTEGER NOT NULL,ClosePrice INTEGER NOT NULL,MaxPrice INTEGER NOT NULL,MinPrice INTEGER NOT NULL,AveragePrice INTEGER NOT NULL,BestPriceBuy INTEGER NOT NULL,BestPriceSale INTEGER NOT NULL,NumberOfBusiness INTEGER NOT NULL,TotalQuantityRoleMarket INTEGER NOT NULL,TotalVolumeRoleMarket TotalQuantityRoleMarket INTEGER NOT NULL)'
+	sQLite.ExecuteNonQuery(sql)
 
-def Open():
-	global conn
-	conn = sqlite3.connect(CONST_NAME_DATABASE)
-	global cur
-	cur = conn.cursor()
-	print("Open")
-def Close():
-	conn.commit()
-	cur.close()
-	conn.close()
-	print("Close")
-def ExecuteQuery(query):
-	#conn = sqlite3.connect(CONST_NAME_DATABASE)
-	#cur = conn.cursor()
-	cur.execute(query)
-	rows = cur.fetchall()
-	#cur.close()
-	conn.commit()
-	return rows
+def InsertDailyStockInfo(idStock, dailyStockInfo):
+	sql =" SELECT ID FROM DailyStockInfo WHERE IDStock="+str(idStock)+" AND Day='"+dailyStockInfo.Date+"'"
+	rows=sQLite.ExecuteQuery(sql)
+	if rows != []:
+		return rows[0][0]
+	else:
+		sql = "INSERT INTO DailyStockInfo (IDStock,Day,OpenPrice,ClosePrice,MaxPrice,MinPrice,AveragePrice,BestPriceBuy,BestPriceSale,NumberOfBusiness,TotalQuantityRoleMarket,TotalVolumeRoleMarket) VALUES ("+str(idStock)+",'"+dailyStockInfo.Date+"', "+str(dailyStockInfo.OpeningPeice)+", "+str(dailyStockInfo.LastPric)+", "+str(dailyStockInfo.MaximimPrice)+", "+str(dailyStockInfo.MinimumPrice)+", "+str(dailyStockInfo.AveragePrice)+", "+str(dailyStockInfo.BestPriceBuy)+", "+str(dailyStockInfo.BestPriceSale)+", "+str(dailyStockInfo.NumberOfBusiness)+", "+str(dailyStockInfo.TotalQuantityRoleMarket)+", "+str(dailyStockInfo.TotalVolumeRoleMarket)+")" 
+		sQLite.ExecuteNonQuery(sql)
+
+def InsertDailyStockInfoLite(idStock, dailyStockInfo):
+	sql = "INSERT INTO DailyStockInfo (IDStock,Day,OpenPrice,ClosePrice,MaxPrice,MinPrice,AveragePrice,BestPriceBuy,BestPriceSale,NumberOfBusiness,TotalQuantityRoleMarket,TotalVolumeRoleMarket) VALUES ("+str(idStock)+",'"+dailyStockInfo.Date+"', "+str(dailyStockInfo.OpeningPeice)+", "+str(dailyStockInfo.LastPric)+", "+str(dailyStockInfo.MaximimPrice)+", "+str(dailyStockInfo.MinimumPrice)+", "+str(dailyStockInfo.AveragePrice)+", "+str(dailyStockInfo.BestPriceBuy)+", "+str(dailyStockInfo.BestPriceSale)+", "+str(dailyStockInfo.NumberOfBusiness)+", "+str(dailyStockInfo.TotalQuantityRoleMarket)+", "+str(dailyStockInfo.TotalVolumeRoleMarket)+")" 
+	sQLite.ExecuteNonQuery(sql)
+
+		
+#Inset in Database all Daily Stock Info
+def InsertStock(allDailyStockInfo):
+	if len(allDailyStockInfo) == 0:
+		return
+	id=GetStockID(allDailyStockInfo[0])
+	for dailyStockInfo in allDailyStockInfo:
+		InsertDailyStockInfo(id,dailyStockInfo)
 	
-def ExecuteNonQuery(query):
-	cur.execute(query)
-	conn.commit()
+def GetDailyStockInfoInDatabase(idStock):
+	query =" SELECT Day FROM DailyStockInfo WHERE IDStock="+str(idStock)
+	return sQLite.ExecuteQuery(query)
 
+#Used to group data from the same trading codes
+def Get_PTOEXE(E):
+	return E.PTOEXE
+class DayInformation:
+	def __init__(self, phrase):
+		self.RegisterType = phrase[0:2]
+		self.Date = phrase[2:10]
+		self.CodeBDI= phrase[10:12]
+		self.CodeOfTrading= phrase[12:24]
+		self.MarketType	= phrase[24:27]
+		self.ShortName = phrase[27:39]
+		self.SpecificationOfPaper = phrase[39:49]
+		self.DeadlineMarket	= phrase[49:52]
+		self.ReferenceCurrency = phrase[52:56]
+		self.OpeningPeice = phrase[56:69]
+		self.MaximimPrice = phrase[69:82]
+		self.MinimumPrice = phrase[82:95]
+		self.AveragePrice	= phrase[95:108]
+		self.LastPric = phrase[108:121]
+		self.BestPriceBuy = phrase[121:134]
+		self.BestPriceSale = phrase[134:147]
+		self.NumberOfBusiness = phrase[147:152]
+		self.TotalQuantityRoleMarket= phrase[152:170]
+		self.TotalVolumeRoleMarket= phrase[170:188]
+		self.PREEXE	= phrase[188:202]
+		self.INDOPC	= phrase[202:202]
+		self.DATVEN	= phrase[203:211]
+		self.FATCOT	= phrase[211:218]
+		self.FATCOT	= phrase[218:231]
+		self.PTOEXE	= phrase[231:243]
+		self.DISMES	= phrase[243:246]
+
+#Search the stock table for trading codes, if not find an insert in this table and return the ID
+def GetStockID(dailyStockInfo):
+	sql = "SELECT ID FROM Stock WHERE CODE_OF_TRADING = '"+dailyStockInfo.CodeOfTrading+"'" 
+	rows=sQLite.ExecuteQuery(sql)	
+	if rows != []:
+		return rows[0][0]
+	else:
+		sql = "INSERT INTO Stock (SHORT_NAME, CODE_OF_TRADING) VALUES ('"+dailyStockInfo.ShortName+"','"+dailyStockInfo.CodeOfTrading+"')"
+		sQLite.ExecuteNonQuery(sql)
+		return GetStockID(dailyStockInfo)
+
+	
+def AddInDB(dailyStockInfo):
+	id=GetStockID(dailyStockInfo)
+	InsertDailyStockInfo(id,dailyStockInfo)
+
+def LoadFileCallAddInDB(FileName):
+	Alldata = []
+	f = open(FileName)
+	line = f.readline()
+	while line:
+		line = f.readline()
+		DayInformationTemp=DayInformation(line)
+		if(DayInformationTemp.RegisterType =="01"):
+			Alldata.append(DayInformationTemp)
+				
+	f.close()
+	print("Load completed")
+	Current_PTOEXE=""
+	AllDailyStockInfo=[]
+	Alldata.sort(key=Get_PTOEXE)
+	for DayInformationin in Alldata:
+		if Current_PTOEXE != DayInformationin.PTOEXE:
+			InsertStock(AllDailyStockInfo)
+			AllDailyStockInfo.clear()
+		AllDailyStockInfo.append(DayInformationin);
+		Current_PTOEXE=DayInformationin.PTOEXE
+	InsertStock(AllDailyStockInfo)
+
+#Change here the name of database file!!!
+CONST_NAME_DATABASE = 'ActionsInfo.db'
+
+sQLite=SQLite(CONST_NAME_DATABASE)
+sQLite.Open()
+CreateTables()
+#Change here the name of TXT bovespa file!!!
+FileName="COTAHIST_A2018.TXT"
+LoadFileCallAddInDB(FileName)	
+sQLite.Close()
+#input("Press Enter to continue...")
